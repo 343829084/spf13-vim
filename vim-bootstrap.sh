3b015cb01745ccc3f67064a6804770bd81f04985
forhappy <haipingf@gmail.com>
2015-07-02 17:19:07 +0800

    Update vim-bootstrap.sh

--- vim-bootstrap.sh
+++ vim-bootstrap.sh
@@ -18,7 +18,7 @@
 ############################  SETUP PARAMETERS
 app_name='freewheel-vim'
 [ -z "$APP_PATH" ] && APP_PATH="$HOME/.freewheel-vim"
-[ -z "$REPO_URI" ] && REPO_URI='https://github.com/spf13/spf13-vim.git'
+[ -z "$REPO_URI" ] && REPO_URI='https://github.com/forhappy/spf13-vim.git'
 [ -z "$REPO_BRANCH" ] && REPO_BRANCH='3.0'
 debug_mode='0'
 fork_maintainer='0'
